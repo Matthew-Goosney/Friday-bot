8eec4cc3cd3577f29b3a5684cd8ab97cd6d4d2bc
Matthew Goosney <thwiyte@gmail.comm>
2024-01-06 14:25:15 -0800

    OpenAI code

--- Matthew/openai.py
+++ Matthew/openai.py
@@ -6,8 +6,15 @@ load_dotenv()
 
 openai.api_key = os.getenv('API_KEY')
 
-def chat_gpt_response(prompt)
+def chat_gpt_response(prompt):
     response = openai.Completion.create(
-        
-
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=1,
+    max_tokens=100    
     )
+
+    response_dict = response.get("choice")
+    if response_dict and len(response_dict) > 0:
+        prompt_response = response_dict[0]["text"]
+    return prompt_response

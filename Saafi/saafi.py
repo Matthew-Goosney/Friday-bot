1eb337e89ea655e718367e0e59a91397f5dd9c8a
SaafiRahman <saafirah@student.ubc.ca>
2024-01-06 14:53:15 -0800

    new  BOTToken

--- Saafi/saafi.py
+++ Saafi/saafi.py
@@ -1,10 +1,10 @@
 from discord.ext import commands
 import discord
 
-BOT_TOKEN = "NDI1Mzc1OTk3NTAwNTg4MDQ0.GGTy2f.y864YJPSz18uYlKKlNcGODK-9k6y3aIJx8eU3E"
+BOT_TOKEN = "MTE5MzI3NzQ4NjUxMTk2MDEwNA.Gl7Lhb.bMZdNyLrXWa1nYuQ0zpD-UN0RVQIJi4jS0sZGE"
 bot = commands.Bot(command_prefix=">", intents=discord.Intents.all())
 
-with open('constantSwears.txt', 'r') as f:
+with open('./constantSwears.txt', 'r') as f:
     global swears
     words = f.read()
     swears = words.split()
@@ -14,13 +14,14 @@ async def on_ready():
     print("bot is made")
 
 @bot.event
-async def on_message(ctx, message):
+async def on_message(message):
     message_content = message.content
     for word in swears:
         if word in message_content:
+            print(word)
             await message.delete()
-            await ctx.send("Dont use that word!")
-    await ctx.process_message(message)
+            await message.channel.send("Don't use that word!")
+    await bot.process_commands(message)
     
 
 bot.run(BOT_TOKEN)

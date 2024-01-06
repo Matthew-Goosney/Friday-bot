2d9f22c829097a3d122e38734f8c86d03c3bff60
Matthew Goosney <thwiyte@gmail.comm>
2024-01-06 13:10:33 -0800

    Changed token in saafi.py

--- Saafi/saafi.py
+++ Saafi/saafi.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord
 
-BOT_TOKEN = "NDI1Mzc1OTk3NTAwNTg4MDQ0.GW2cgQ.WnoNLz15RGEEnpoaHoQ0Q28CAKtP38mu_yIytA"
+BOT_TOKEN = "NDI1Mzc1OTk3NTAwNTg4MDQ0.GepsGZ.-gcWUM3t17DMVJfyz3qFSaaa-vR0T2yD-gJcRQ"
 bot = commands.Bot(command_prefix=">", intents=discord.Intents.all())
 
 @bot.event
